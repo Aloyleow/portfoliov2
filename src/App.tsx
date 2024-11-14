76e88c2778b1f87572fca03e0b9f8d87dbc34a90
Aloy <aloyleow91@gmail.com>
2024-11-14 20:54:17 +0800

    feat: created mainpage and installed necessary packages

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { Routes, Route } from "react-router-dom"
+import MainPage from "./pages/main/MainPage"
+
+function App() {
+  
+
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<MainPage/>}/>
+      </Routes>
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/main/MainPage.tsx
@@ -0,0 +1,9 @@
+const MainPage = () => {
+  return (
+    <>
+      <h1>Hi</h1>
+    </>
+  )
+}
+
+export default MainPage
